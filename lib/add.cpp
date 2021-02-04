92480f8c0e20ff7822c6965dc88e2208b0114322
root <cnandrelee@gmail.com>
2021-02-05 01:46:22 +0800

    add lib

---
+++ lib/add.cpp
@@ -0,0 +1,8 @@
+#include "add.h"
+#include <iostream>
+using namespace std;
+
+int add ( int x , int y ) {
+    return x + y ;
+}
+
--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "HelloWorld.h"
 #include "fun.h"
+#include "../lib/add.h"
 using namespace std;
 
 int main()
@@ -8,6 +9,7 @@ int main()
     std:cout<<"this is HelloWorld.cpp"<<endl;
     hello();
     fun();
+    cout << add(3,6) << endl;
     return 0;
 }
 
